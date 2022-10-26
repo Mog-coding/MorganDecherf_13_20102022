5f4fffa711b2d9d15b916060e6901a712bebba58
Morgan <morgand@tutamail.com>
2022-10-26 13:10:42 +0200

    change css structure

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import HomePage from './pages/HomePage/HomePage';
-import SignInPage from './pages/SignInPage/SignInPage';
-import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './index.css';
+import Header from './components/Header/Header';
+import SignInPage from './pages/SignInPage/SignInPage';
+import UserPage from './pages/UserPage/UserPage';
+import Footer from './components/Footer/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-  <Router>
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/sign" element={<SignInPage />} />
-    </Routes>
-  </Router>
-  //</React.StrictMode>
+    //<React.StrictMode>
+    <Router>
+        <Header />
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/sign" element={<SignInPage />} />
+            <Route path="/user" element={<UserPage />} />
+        </Routes>
+        <Footer />
+    </Router>
+    //</React.StrictMode>
 );
