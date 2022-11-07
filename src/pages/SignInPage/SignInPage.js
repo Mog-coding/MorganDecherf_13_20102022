2d103271bf7280b6fa7a7eae3338308be403b921
Morgan <morgand@tutamail.com>
2022-11-07 12:34:52 +0100

    axios post signInPage

--- src/pages/SignInPage/SignInPage.js
+++ src/pages/SignInPage/SignInPage.js
@@ -1,35 +1,68 @@
+import { useState } from 'react';
 import './SignInPage.css';
+import axios from "axios";
+import { useDispatch } from 'react-redux';
+import { setTokenData } from '../../feature/tokenSlice';
 
 export default function SignInPage() {
+    const dispatch = useDispatch();
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('handleUser', userName);
+        console.log('handlePass', password);
+        axios
+        .post("http://localhost:3001/api/v1/user/login", {
+            email: userName,
+            password: password
+        })
+        .then((resp) => { 
+            console.log("resp", resp);
+            console.log("token", resp.data.body )
+            dispatch(setTokenData(resp.data.body))
+    })
+    };
 
     return (
         <>
             <main className="mainSign bg-dark">
+                {console.log('render')}
                 <section className="sign-in-content">
                     <i className="fa fa-user-circle sign-in-icon"></i>
                     <h1>Sign In</h1>
-                    <form>
+
+                    <form onSubmit={(e) => handleSubmit(e)}>
                         <div className="input-wrapper">
                             <label htmlFor="username">Username</label>
-                            <input type="text" id="username" />
+                            <input
+                                type="text"
+                                id="username"
+                                onChange={(e) => {
+                                    setUserName(e.target.value);
+                                }}
+                            />
                         </div>
                         <div className="input-wrapper">
                             <label htmlFor="password">Password</label>
-                            <input type="password" id="password" />
+                            <input
+                                type="password"
+                                id="password"
+                                onChange={(e) => {
+                                    setPassword(e.target.value);
+                                }}
+                            />
                         </div>
                         <div className="input-remember">
-                            <input type="checkbox" id="remember-me" />
                             <label htmlFor="remember-me">Remember me</label>
-
+                            <input type="checkbox" id="remember-me" />
                         </div>
-                        {/* <!-- PLACEHOLDER DUE TO STATIC SITE --> */}
-                        <a href="./user.html" className="sign-in-button">Sign In</a>
-                        {/* <!-- SHOULD BE THE BUTTON BELOW -->
-          <!-- <button className="sign-in-button">Sign In</button> -->
-          <!--  --> */}
+
+                        <button className="sign-in-button">Sign In</button>
                     </form>
                 </section>
             </main>
         </>
-    )
+    );
 }
