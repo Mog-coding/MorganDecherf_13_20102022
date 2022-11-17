2c662a2f1d5433ad3d7f8b47d08e38417de47e40
Morgan <morgand@tutamail.com>
2022-11-17 14:37:52 +0100

    remove console.log

--- src/reducers/authentReducer.js
+++ src/reducers/authentReducer.js
@@ -6,10 +6,8 @@ const initState = {
 export const authentReducer = (state = initState, action) => {
     switch (action.type) {
         case "SIGN_IN_SUCCESS":
-            console.log("action", action)
             return { ...state, token: action.payload }
         case "SIGN_IN_ERROR":
-            console.log("action", action)
             return { ...state, errorMessage: action.payload }
         default:
             return state
