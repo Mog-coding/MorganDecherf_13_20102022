631c1ba88d210423a27a216f6cc78fd7cb46cf49
Morgan <morgand@tutamail.com>
2022-11-30 09:20:55 +0100

    factorisation isTokenValid

--- src/utils/isTokenValid.js
+++ src/utils/isTokenValid.js
@@ -2,15 +2,7 @@ export const isTokenValid = (JWT) => {
 
     if(!JWT) return false
 
-    console.log("type token", typeof JWT);
-    console.log("token", JWT)
-
     const jwtPayload = JSON.parse(window.atob(JWT.split('.')[1]))
 
-
-    if ((new Date().getTime() / 1000) < jwtPayload.exp) {
-        return true
-    } else {
-        return false
-    }
+    return (new Date().getTime() < jwtPayload.exp * 1000)
 }
